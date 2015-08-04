d257908f39b63f4b78439734f394a824c9b6bcb9
Tim Schmelmer <tim.schmelmer@livingsocial.com>
2015-08-03 20:44:29 -0600

    solved alphabet-cipher

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,7 +1,33 @@
 (ns alphabet-cipher.coder)
 
-(defn encode [keyword message]
-  "encodeme")
+(defn rotations
+  [xs]
+  (take (count xs) (partition (count xs) 1 (cycle xs))))
 
-(defn decode [keyword message]
-  "decodeme")
+(def a-to-z (map char (range (int \a) (-> \z int inc))))
+
+(def alphabet-matrix-map (->> a-to-z
+                              rotations
+                              (interleave a-to-z)
+                              (apply assoc {})))
+
+(defn encode-letter
+  [key letter]
+  (-> alphabet-matrix-map
+      (get key)
+      (nth (- (int letter) (int \a)))))
+
+(defn encode
+  [keyword message]
+  (let [kw-to-use (take (count message) (cycle keyword))]
+    (->> message
+         seq
+         (map encode-letter kw-to-use)
+         (apply str))))
+
+(defn decode
+  [keyword message]
+  (->> (encode keyword message)
+       (iterate #(encode keyword %))
+       (take 25)
+       last))
