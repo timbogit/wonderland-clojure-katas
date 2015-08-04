1829e3cc39c24fdd2423c50917d24cafa86ff1de
Tim Schmelmer <tim.schmelmer@livingsocial.com>
2015-08-03 21:37:42 -0600

    solved wonderland-number

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,22 @@
 (ns wonderland-number.finder)
 
+
+(defn same-digits? [n1 n2]
+  (let [s1 (set (str n1))
+        s2 (set (str n2))]
+    (= s1 s2)))
+
+(def candidates (range 100000 (/ 1000002 6)))
+
 (defn wonderland-number []
   ;; calculate me
-  42)
+  (loop [candidate (first candidates)
+         others    (rest candidates)]
+    (if (and (= 6 (count (str candidate)))
+             (same-digits? candidate (* 2 candidate))
+             (same-digits? candidate (* 3 candidate))
+             (same-digits? candidate (* 4 candidate))
+             (same-digits? candidate (* 5 candidate))
+             (same-digits? candidate (* 6 candidate)))
+      candidate
+      (recur (first others) (rest others)))))
