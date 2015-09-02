83b275d879f4b3a88d16c90e201ffb5b28ef85a6
Tim Schmelmer <tim.schmelmer@livingsocial.com>
2015-09-01 21:42:43 -0600

    solved fox-goose-bag-of-corn

--- fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
+++ fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
@@ -2,5 +2,25 @@
 
 (def start-pos [[[:fox :goose :corn :you] [:boat] []]])
 
+(def other-steps
+  [[[:fox :corn] [:boat :you :goose] []]
+   [[:fox :corn] [:boat] [:goose :you]]
+   [[:fox :corn] [:boat :you] [:goose]]
+   [[:fox :corn :you] [:boat] [:goose]]
+   [[:corn] [:boat :you :fox] [:goose]]
+   [[:corn] [:boat] [:goose :you :fox]]
+   [[:corn] [:boat :fox :you] [:goose]]
+   [[:corn :fox :you] [:boat] [:goose]]
+   [[:fox] [:boat :corn :you] [:goose]]
+   [[:fox] [:boat] [:goose :corn :you]]
+   [[:fox] [:boat :goose :you] [:corn]]
+   [[:fox :goose :you] [:boat] [:corn]]
+   [[:goose] [:boat :fox :you] [:corn]]
+   [[:goose] [:boat] [:corn :fox :you]]
+   [[:goose] [:boat :you] [:corn :fox]]
+   [[:goose :you] [:boat] [:corn :fox]]
+   [[] [:boat :you :goose] [:corn :fox]]
+   [[] [:boat] [:corn :fox :you :goose]]])
+
 (defn river-crossing-plan []
-  start-pos)
+  (into start-pos other-steps))
